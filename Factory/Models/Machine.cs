621c4cea793eb5ee2b0a5f7c80a80f65002cc865
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-03-17 14:59:54 -0700

    WIP: fix error that prevents machine page from working

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,11 +1,15 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
 
 namespace Factory.Models
 {
   public class Machine
   {
     public int MachineId { get; set; }
+    [Required(ErrorMessage = "The Machine name field can't be empty!")]
     public string MachineName { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "You must add an engineer to a machine. Have you added a machine yet?")]
     public string MachineDetails { get; set; }
     public List<EngineerMachine> JoinEntities { get;}
   }
